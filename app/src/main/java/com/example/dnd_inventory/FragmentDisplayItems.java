7b9a552a2f8f98917fb9c7407c7cb317a0a5a62a
EvanKards42 <Evankardos37@gmail.com>
2019-02-16 20:57:07 -0800

    Finished up add item layout and started making other fragments so that we can get data for very specific items with different attributes

---
+++ app/src/main/java/com/example/dnd_inventory/FragmentDisplayItems.java
@@ -0,0 +1,16 @@
+package com.example.dnd_inventory;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+
+public class FragmentDisplayItems extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
